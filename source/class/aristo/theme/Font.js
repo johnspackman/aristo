0a5c2b7ce6d68d6d69e3e9a536321a2c0e58274d
John Spackman <john.spackman@zenesis.com>
2014-02-25 18:30:10 +0000

    font

--- source/class/aristo/theme/Font.js
+++ source/class/aristo/theme/Font.js
@@ -8,65 +8,58 @@
      EPL: http://www.eclipse.org/org/documents/epl-v10.php
 
    Authors:
-     * Guilherme R. Aiolfi (guilhermeaiolfi)
-     * John Spackman (john.spackman@zenesis.com)
+ * Guilherme R. Aiolfi (guilhermeaiolfi)
+ * John Spackman (john.spackman@zenesis.com)
 
    ======================================================================
 
    This class contains code and resources based on the following work:
 
-   * Aristo
+ * Aristo
      http://github.com/280north/aristo
 
      License:
        http://creativecommons.org/licenses/by-sa/3.0/us/
 
      Authors:
-       * 280 North, Inc., http://280north.com/
-       * Sofa, http://madebysofa.com/
+ * 280 North, Inc., http://280north.com/
+ * Sofa, http://madebysofa.com/
 
-************************************************************************ */
+ ************************************************************************ */
 
 /**
  * The Aristo font theme.
  */
-qx.Theme.define("aristo.theme.Font",
-{
-  fonts :
-  {
-    "default" :
-    {
-      size : 12,
-      lineHeight : 1.4,
-      family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
+qx.Theme.define("aristo.theme.Font", {
+  fonts: {
+    "default": {
+      size: 12,
+      lineHeight: 1.4,
+      family: [ "Open Sans", "Segoe UI", "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
-    "bold" :
-    {
-      size : 12,
-      lineHeight : 1.4,
-      family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ],
-      bold : true
+    "bold": {
+      size: 12,
+      lineHeight: 1.4,
+      family: [ "Open Sans", "Segoe UI", "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ],
+      bold: true
     },
 
-    "small" :
-    {
-      size : 11,
-      lineHeight : 1.4,
-      family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
+    "small": {
+      size: 11,
+      lineHeight: 1.4,
+      family: [ "Open Sans", "Segoe UI", "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
-    "monospace" :
-    {
-      size : 12,
-      lineHeight : 1.4,
-      family : [ "Courier New", "DejaVu Sans Mono", "monospace" ]
+    "monospace": {
+      size: 12,
+      lineHeight: 1.4,
+      family: [ "Courier New", "DejaVu Sans Mono", "monospace" ]
     },
-    
-    "headline" :
-    {
-      size : 24,
-      family : ["sans-serif", "arial"]
+
+    "headline": {
+      size: 24,
+      family: [ "Open Sans", "Segoe UI", "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     }
 
   }
