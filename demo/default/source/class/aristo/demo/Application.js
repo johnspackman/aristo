984d9aa3ac2f357a717dab551a48c30b39294cac
John Spackman <john.spackman@zenesis.com>
2014-03-05 18:00:20 +0000

    test code

--- demo/default/source/class/aristo/demo/Application.js
+++ demo/default/source/class/aristo/demo/Application.js
@@ -447,7 +447,7 @@ qx.Class.define("aristo.demo.Application", {
         widgetButton.setSelection([ selWidgetButton ]);
 
       this.closeShowcase();
-      this.stack.setSelection([ selectBoxWidgets ]);
+      this.stack.setSelection([ tabViewWidgets ]);
     },
 
     createHeader : function() {
