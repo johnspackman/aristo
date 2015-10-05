a2c6af9bc632d77acfcbdc419667cab314b2ad15
John Spackman <john.spackman@zenesis.com>
2015-10-05 15:10:56 +0100

    comments correction

--- demo/default/source/class/aristo/demo/Application.js
+++ demo/default/source/class/aristo/demo/Application.js
@@ -1,15 +1,31 @@
 /* ************************************************************************
-
-   Copyright:
-     2010 Norbert Schröder
-
-   License:
-     LGPL: http://www.gnu.org/licenses/lgpl.html
-     EPL: http://www.eclipse.org/org/documents/epl-v10.php
-
-   Authors:
- * Norbert Schröder (scro34)
-
+ *
+ *   Copyright:
+ *     2010 Guilherme R. Aiolfi
+ *     2012 Zenesis Ltd
+ *
+ *   License:
+ *     LGPL: http://www.gnu.org/licenses/lgpl.html
+ *     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ *   Authors:
+ *     Guilherme R. Aiolfi (guilhermeaiolfi)
+ *     John Spackman (john.spackman@zenesis.com)
+ *
+ *   ======================================================================
+ *
+ *   This class contains code and resources based on the following work:
+ *
+ *   Aristo
+ *       http://github.com/280north/aristo
+ *
+ *   License:
+ *       http://creativecommons.org/licenses/by-sa/3.0/us/
+ *
+ *   Authors:
+ *       280 North, Inc., http://280north.com/
+ *       Sofa, http://madebysofa.com/
+ *
  ************************************************************************ */
 
 /**
